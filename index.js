50f10b212761b7823102c03fc8fc0df0d63e30a3
John <johnsuyang2118@gmail.com>
2024-09-08 18:32:43 +0800

    Remove all comments

--- index.js
+++ index.js
@@ -16,11 +16,8 @@ class LinkedList {
     }
 
     append(value) {
-        //  adds a new node containing value to the end of the list
-
         if (!value) return;
         const newNode = new Node(value);
-        // if the size of the list is < 1, set both the head and tail to the node
         if (this.size < 1) {
             this.head = newNode;
             this.tail = newNode;
@@ -31,14 +28,11 @@ class LinkedList {
         }
 
         this.size++;
-        // else just update the tail to the current node and increment the size.
     }
 
     prepend(value) {
-        // adds a new node containing value to the start of the list
         if (!value) return;
         const newNode = new Node(value);
-
         if (this.size < 1) {
             this.head = newNode;
             this.tail = newNode;
@@ -47,131 +41,70 @@ class LinkedList {
             this.head = newNode;
             this.head.next = currentHead;
         }
-
-        // if the size of the list is < 1
-        //      it means that the value to be added will be the head of the list. as well as the tail.
-        //      increment the size
-        // else
-        //      it means the value will be replace the current head as the new head
-        //      then the newHead.next will be the current.head
-        //      increment the size.
-
         this.size++;
     }
 
     at(index) {
         if (index > 0) {
             if (index == 1) return this.head;
-
             let i = 1;
             let targetNode = this.head;
-
             while (i < index) {
                 targetNode = targetNode.next;
                 i++;
             }
-
             return targetNode;
         }
         return null;
     }
 
     pop() {
-        //removes the last element from the list;
-
         if (this.size < 1) return;
-
-        // node before the tail
         const newTail = this.at(this.size - 1);
         newTail.next = null;
         this.tail = newTail;
         this.size--;
-        /**
-         * if the size is < 1
-         *      then do nothing
-         * else
-         *      traverse from the head to the node before the tail.
-         *      set the currentNode.next to null
-         *      set the current node as the new tail.
-         *      and decrease the size
-         */
     }
 
     contains(value) {
         if (!value) return false;
-        //returns true if the passed in value is in the list and otherwise returns false.
-
-        /**
-         * loop through the list starting from the head up to the tail
-         * each loop. make sure to check if the value is equal to the data of the current node.
-         */
         let tempNode = this.head;
-
         while (tempNode) {
             if (tempNode.data == value) {
                 return true;
             }
-
             tempNode = tempNode.next;
         }
-
         return false;
     }
 
     find(value) {
-        // returns the index of the node containing value, or null if not found.'
-        /**
-         * loop through the list starting from the head
-         */
-
         if (!value) return null;
-
-        /**
-         * loop through the list starting from the head up to the tail
-         * each loop. make sure to check if the value is equal to the data of the current node.
-         */
         let tempNode = this.head;
         let counter = 0;
-
         while (tempNode) {
             if (tempNode.data == value) {
                 return counter;
             }
-
             tempNode = tempNode.next;
             counter++;
         }
-
         return null;
     }
 
     toString() {
-        // represents your LinkedList objects as strings, so you can print them out and preview them in the console. The format should be: ( value ) -> ( value ) -> ( value ) -> null
         let string = "";
         let tempNode = this.head;
-
         while (tempNode) {
             string += `( ${tempNode.data} ) -> `;
             tempNode = tempNode.next;
         }
         string += "( null )";
-        console.log(string);
         return string;
     }
 
     insertAt(value, index) {
         if (!value || index < -1) return;
-
-        // that inserts a new node with the provided value at the given index.
-        /**
-         *  if the index is < the size - 1
-         *      the node before the targetNode will be need to update the .next = newNode
-         *      then the newNode.next will be the node at the current index
-         *      update the size
-         *
-         *  if the index provide is > than the size - 1. just use the append.
-         *  if the index provide = 0 then just use the prepend()
-         */
 
         if (index == 0) {
             this.prepend(value);
@@ -185,11 +118,6 @@ class LinkedList {
 
         if (index <= this.size) {
             const newNode = new Node();
-            /**
-             * the node before the targetNode will be need to update the .next = newNode
-             *      then the newNode.next will be the node at the current index
-             *      update the size
-             */
             let nodeBeforeTarget;
             let tempNode = this.head;
             let i = 1;
@@ -203,7 +131,6 @@ class LinkedList {
                 i++;
             }
 
-            // at the end of the loop. the tempNode will be equal to the target node
             nodeBeforeTarget.next = newNode;
             newNode.next = tempNode;
             this.size++;
@@ -212,7 +139,6 @@ class LinkedList {
 
     removeAt(index) {
         if (index < 1 || index > this.size) return;
-        // that removes the node at the given index.
 
         if (index == 1) {
             const newHead = this.head.next;
@@ -233,7 +159,6 @@ class LinkedList {
             tempNode = tempNode.next;
             i++;
         }
-        // after the loop the tempNode is equal to the node to delete
 
         if (index == this.size) {
             nodeBeforeTarget.next = null;
@@ -244,45 +169,14 @@ class LinkedList {
         }
 
         this.size--;
-        /**
-         * if the index is the last index
-         *      loop the list from the head until the node before the tail
-         *      set the .next of that node to null
-         *      descrease the size
-         *
-         * if the index == 1
-         *      get the next node using head.next = newHead
-         *      set the head.next to null
-         *      set as the this.head = newHead
-         *
-         *  else
-         *      get the node before the target = nodeBeforeTarget
-         *      get the node after the target = nodeAfterTarget
-         *      set nodeBeforeTarget.next = nodeAfterTarget
-         *      set target.next = null
-         *
-         */
     }
 }
 
-const List = new LinkedList();
-List.append("Dog");
-List.prepend("Cat");
-List.append("Tiger");
-List.append("Elephant");
-List.append("Bird");
-List.toString();
-List.removeAt(List.size);
-List.toString();
-List.removeAt(1);
-List.toString();
-List.removeAt(2);
-List.toString();
-// List.insertAt("Lion", 0);
-// List.insertAt("Elephant", List.size);
-// List.toString();
-// List.insertAt("Bird", List.size - 1);
-// List.contains("Dog");
-// List.toString();
-// List.pop();
-// List.toString();
+const list = new LinkedList();
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+console.log(list.toString());
